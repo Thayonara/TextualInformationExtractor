f9c780c160918cda6458dfd636c27853b1b0f6d7
Eduardo <eduardohmrodrigues@gmail.com>
2017-05-27 10:32:48 -0300

    Project Created

---
+++ src/Wrapper/ETextField.java
@@ -0,0 +1,11 @@
+package Wrapper;
+
+public enum ETextField {
+	 Authors,
+	 Title,
+	 Subjects,
+	 Comments,
+	 Link,
+	 Relations,
+	 ProcessedRelations
+}
---
+++ src/Wrapper/TextPreprocessor.java
@@ -0,0 +1,19 @@
+package Wrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TextPreprocessor {
+	public static Map<ETextField, String> ProcessText(String text){
+		Map<ETextField, String> map = new HashMap<ETextField, String>();
+		
+		
+		
+		return map;
+	}
+	
+	public static String GetResult(Map<ETextField, String> map){
+		
+		return "";
+	}
+}
